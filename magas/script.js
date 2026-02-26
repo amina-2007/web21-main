e6a23ab508b6df69bea09e5eb019b8cb45404c73
amina-2007 <aminagatagova@mail.ru>
2026-02-26 10:21:08 +0300

    tes

---
+++ magas/script.js
@@ -0,0 +1,9 @@
+const form=document.querySelector('#recipe-form');
+const titleInput=document.querySelector('#title');
+const descriptionInput=document.querySelector('#description');
+const difficultySelect=document.querySelector('#difficulty');
+const imageInput=document.querySelector('#image');
+const timeInput=document.querySelector('#time');
+const ingredientsInput=document.querySelector('#ingredients');
+const list=document.querySelector('#recipe-list');  
+
